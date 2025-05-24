2c5b184f8813c1ef1c02d586c3d913cdd0ca5c2f
RamaFitantra <ramafitantra@gmail.com>
2025-05-24 09:30:14 +0700

    Re-push livewire-compro project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,12 @@
-import './bootstrap';
-import '../css/app.css';
+// resources/js/app.js
+import './bootstrap';  // Jika ada file bootstrap atau pengaturan lainnya
+
+// Fungsi untuk scroll
+window.onscroll = function() {
+    var nav = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        nav.classList.add('bg-opacity-90');
+    } else {
+        nav.classList.remove('bg-opacity-90');
+    }
+};
